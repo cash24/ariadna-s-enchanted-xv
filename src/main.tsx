1df0f6503d25868c21f4d94944acaef8b1683776
JOSE MIGUEL URBINA JUAN <jurbina@elektra.com.mx>
2026-02-16 18:18:06 -0600

    feat: Implement React app initialization and `FooterSection` component, and update `index.html` for version 1.4.8 and loader enhancements.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root")!);
-if ((window as any).log) (window as any).log("App React 1.4.7 OK.");
 window.APP_INITIALIZED = true;
+if ((window as any).log) (window as any).log("📦 App 1.4.8 Lanzada");
+
+const root = createRoot(document.getElementById("root")!);
 root.render(<App />);
